160572a3a2280d69497b18a567fe5be4437473c6
gokhan.gungor@doa.com.tr <gokhan.gungor@doa.com.tr>
2023-09-08 16:08:39 +0300

    Senkron ve Asenkron Repository

---
+++ corePackages/Core.Packages/Core.Persistence/Paging/Paginate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Paging;
+
+public class Paginate<T>
+{
+    public Paginate()
+    {
+        Items = Array.Empty<T>();
+    }
+
+    public int Size { get; set; }  //Kac data var
+    public int Index { get; set; }  //Hangi sayfadayiz
+    public int Count { get; set; }  //Toplan Kayit Sayisi
+    public int Pages { get; set; }  //Toplam Kac Sayfa var
+
+    public IList<T> Items { get; set; }
+
+    public bool HasPrevious => Index > 0;
+    public bool HasNext => Index + 1  < Pages; 
+}
---
+++ corePackages/Core.Packages/Core.Persistence/Respositories/IQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Respositories
+{
+    public interface IQuery<T>
+    {
+        //Sql sorgularının TSql yazildgi kisim
+        IQueryable<T> Query();
+    }
+}
