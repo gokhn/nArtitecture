09b751a2714f7a66e3e87ccca289e2f2063d6f79
Gokhan Gungor <gokhngungor@gmail.com>
2024-06-29 00:40:47 +0300

    3.2.6 Dynamic Implementasyonu

--- rentACar/RentACar/Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
+++ rentACar/RentACar/Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3fa47383a4cf1748451afa62c9d2adeb5134c57")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- rentACar/RentACar/Infrastructure/obj/Debug/net7.0/Infrastructure.AssemblyInfo.cs
+++ rentACar/RentACar/Infrastructure/obj/Debug/net7.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3fa47383a4cf1748451afa62c9d2adeb5134c57")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
