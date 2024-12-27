09b988f644f5d389d4cf326e40c64df55e11e523
Gokhan <gokhngungor@gmail.com>
2024-12-28 01:16:47 +0300

    Mediatr PipeLine Validation Implementation

--- corePackages/Core.Packages/Core.Application/obj/Debug/net8.0/Core.Application.AssemblyInfo.cs
+++ corePackages/Core.Packages/Core.Application/obj/Debug/net8.0/Core.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e17afed7e46688568a08c7c0a54cca588367f944")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1521c6f3e28de7754f389b271421df118ae80168")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
